8f0e6b48e523ede1b98fd5fe9774c79be6882cd3
“Benjamin <2856228D@student.gla.ac.uk>
2024-02-08 15:41:49 +0000

    finished chapter 6 with tests working

--- rango/views.py
+++ rango/views.py
@@ -6,11 +6,13 @@ from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage matches to {{ boldmessage }} in the template! 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier. # Note that the first parameter is the template we wish to use. 
     return render(request, 'rango/index.html', context=context_dict)
