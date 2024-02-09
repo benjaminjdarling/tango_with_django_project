c65b1fd73edaa1bbac2462917049a334c5048577
“Benjamin <2856228D@student.gla.ac.uk>
2024-02-09 18:26:26 +0000

    all 9 working except <title> for register

--- rango/views.py
+++ rango/views.py
@@ -64,7 +64,7 @@ def show_category(request, category_name_slug):
         # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
 
-
+@login_required
 def add_category(request): 
     form = CategoryForm()
 
@@ -87,7 +87,7 @@ def add_category(request):
     # Will handle the bad form, new form, or no form supplied cases. # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
 
-
+@login_required
 def add_page(request, category_name_slug): 
 
     try:
@@ -212,7 +212,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 
 @login_required
